198eb88d4e311bbc6d4227e171b089abb01e3d52
fernando <ferror18@gmail.com>
2019-06-06 22:18:33 -0700

    added services, contact, about template with urls. (issue with the navbar still remains

--- Djcruz/urls.py
+++ Djcruz/urls.py
@@ -19,5 +19,8 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('services/', views.services, name="services"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def about(request):
+    return render(request, 'about.html')
