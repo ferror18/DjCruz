5df4f2fc57e6d89b3d0ddae2c40c438223ba601a
fernando <ferror18@gmail.com>
2019-07-22 23:05:10 -0700

    c

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import reservation
 
+HRS_CHOICES = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+
 class reservationform(forms.ModelForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
